31afd01d536d46640a88a04669778e98bc6369f4
Oscar Torres <0271975@up.edu.mx>
2025-12-02 20:45:25 -0600

    ProyectoFinalTerminado

--- MapaTraficoGdl.py
+++ MapaTraficoGdl.py
@@ -1,50 +1,99 @@
-import pandas as pd
 import streamlit as st
-import plotly.express as px
+import pandas as pd
+import time
 
 st.set_page_config(
-    page_title="Tráfico en la ZMG",
-    layout="wide"
+    page_title = "Mapa de Trafico en la ZMG",
+    layout = "wide"
 )
 
-def ObtenerColor(color_texto):
-    """
-    Convierte el nombre del color del tráfico a una lista RGB [R, G, B].
-    """
-    if color_texto == 'red':
-        # Rojo (Congestión)
-        return [240, 0, 0]
-    elif color_texto == 'green':
-        # Verde (Flujo libre)
-        return [20, 217, 4]
-    elif color_texto == 'orange':
-        # Amarillo (Tráfico lento)
-        return [245, 230, 11]
-    elif color_texto == 'red_wine':
-         return [163, 11, 8]
-    else:
-        # Gris (Para datos no definidos o nulos)
-        return [150, 150, 150]
+st.title("Trafico por hora en la ZMG")
 
-@st.cache_data
-def CargarDatos(ruta):
-    df = pd.read_csv(ruta)
-    df['timestamp'] = pd.to_datetime(['timestamp'])
-    df['color'] = df['predominant_color'].apply(ObtenerColor)
-    return df
-
-HorasUnicas =  sorted(DatosTrafico['timestamp'].dt.floor('H').unique())
-
-ArchivoDatosLimpios = 'locationPointsLimpioFinal.csv'
+st.sidebar.markdown("Musica")
 
 try:
-        datos_trafico = CargarDatos(ArchivoDatosLimpios)
-except FileNotFoundError:
-      st.error(f"Error: No se encontró el archivo {ArchivoDatosLimpios}.")
-      st.stop()
+    st.audio("POOsong.mp3", format = "audio/mp3")
+except:
+    st.sidebar.warning("No se encontro el archivo de musica")
 
-st.title("Trafico con el tiempo en la ZMG")
+def ColorPuntos(color_texto):
+    if color_texto == 'red_wine':
+        return [130,0,180]
+    elif color_texto == 'red':
+        return [230,11,6]
+    elif color_texto == 'orange':
+        return [230,120,20]
+    elif color_texto == 'green':
+        return [24,217,30]
+    else:
+        return [150,150,150]
+    
+@st.cache_data
+def CargarDatos(ruta):
+    try:
+        # Leer archivo
+        df = pd.read_csv(ruta)
 
-MapPlaceholder = st.empty()
-TimeTextPlaceholder = st.empty()
+        # Convertir la columna de texto a tiempo real
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
 
+        # Aplicar colores
+        df['color'] = df['predominant_color'].apply(ColorPuntos)
+        return df
+
+    except FileNotFoundError:
+        return None
+
+ARCHIVO = 'locationPointsLimpioFinal.csv'
+datos_trafico = CargarDatos(ARCHIVO)
+
+if datos_trafico is None:
+    st.error(f"No se encontro el archivo: {ARCHIVO}")
+    st.stop()
+
+horas_unicas = sorted(datos_trafico['timestamp'].dt.floor('h').unique())
+
+TextoHora = st.empty()
+MapaContenedor = st.empty()
+
+# Elegir velocidad
+OpcionVelocidad = st.select_slider(
+    "Selecciona la velocidad deseada:",
+    options = ["Lento", "Normal", "Rapido", "Muy Rapido"],
+    value = "Normal"
+)
+if OpcionVelocidad == "Lento":
+    tiempo_espera = 0.8
+elif OpcionVelocidad == "Normal":
+    tiempo_espera = 0.5
+elif OpcionVelocidad == "Rapido":
+    tiempo_espera = 0.2
+elif OpcionVelocidad == "Muy Rapido":
+    tiempo_espera = 0.05
+
+
+if st.button ('Ver trafico'):
+    BarraProgreso = st.progress(0)
+    TotalPasos = len(horas_unicas)
+
+    for i, hora_actual in enumerate(horas_unicas):
+        TextoHora.subheader(f"Hora: {hora_actual}")
+
+        DatosHora = datos_trafico[datos_trafico['timestamp'].dt.floor('h') == hora_actual]
+
+        if not DatosHora.empty:
+            MapaContenedor.map(
+                DatosHora,
+                latitude='Coordy',
+                longitude='Coordx',
+                color='color',
+                size=20,
+                zoom=11
+            )
+
+        BarraProgreso.progress((i + 1)/ TotalPasos)
+
+        # Segundos por hora
+        time.sleep(tiempo_espera)
+    
+    st.success("Animacon mostrada con exito")
